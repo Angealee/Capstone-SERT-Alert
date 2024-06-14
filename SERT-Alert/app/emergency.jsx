1d3ade0b1c8582aa54a6a2912d11dbb3610a22d3
Koby Macale <koby.macale24@gmail.com>
2024-06-14 18:46:40 +0800

    Created an emergency link for testing

---
+++ SERT-Alert/app/emergency.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const emergency = () => {
+  return (
+    <View>
+      <Text>Incident Report</Text>
+    </View>
+  )
+}
+
+export default emergency
+
+const styles = StyleSheet.create({})
--- SERT-Alert/app/index.js
+++ SERT-Alert/app/index.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Link } from 'expo-router';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>SERT Alert</Text>
       <StatusBar style="auto" />
+      <Link href="/emergency" style={{ color: 'red'}}>Report an Emergency!</Link>
     </View>
   );
 }
