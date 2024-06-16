b87319ed6f6d68a24b7afec9a81e49735a1d4b66
Koby Macale <koby.macale24@gmail.com>
2024-06-16 14:34:10 +0800

    Initial layouts/Home button icon added and fix fonts for home

--- SERT-Alert/app/(tabs)/_layout.jsx
+++ SERT-Alert/app/(tabs)/_layout.jsx
@@ -1,12 +1,43 @@
 import { View, Text } from 'react-native'
 import { Tabs, Redirect } from 'expo-router';
+import { Image } from 'react-native';
 
+
+import  { icons } from '../../constants'
+
+const TabIcon = ({ icon, color, name, focused }) => {
+  return (
+    <View>
+      <Image
+        source={icon}
+        resizeMode="contain"
+        tintColor={color}
+        className="w-6 h-5"
+      />
+      <Text className={`${focused ? 'font-psemibold':'font-pregular'} text-xs` }>
+        {name}
+      </Text>
+    </View>
+  )
+}
 const TabsLayout = () => {
   return (
     <>
         <Tabs>
             <Tabs.Screen 
                 name="home"
+                options={{
+                  title: 'Home',
+                  headerShown: false,
+                  tabBarIcon: ({ color, focused }) => (
+                    <TabIcon
+                      icon={icons.home}
+                      color={color}
+                      name="Home"
+                      focused={focused}
+                    />
+                  )
+                }}
             />
         </Tabs>
     </>
