7a7030a7b99e7b8392890b5894db78fcd0d5f30d
Koby Macale <koby.macale24@gmail.com>
2024-06-17 16:35:18 +0800

    changed sample logo to SERT Alert and Button is functional routing to the next tab

--- SERT-Alert/app/index.jsx
+++ SERT-Alert/app/index.jsx
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, ScrollView, Text, View } from 'react-native';
 import { Image } from 'react-native';
-import { Link } from 'expo-router';
+import { Redirect, router } from 'expo-router';
 
 import { images } from '../constants';
 import CustomButton from '../components/CustomButton';
@@ -14,24 +14,20 @@ export default function App() {
         {/* <Text className="text-3xl font-pblack">SERT Alert</Text>
             <StatusBar style="auto" />
             <Link href="/emergency" style={{ color: 'white'}}>Report an Emergency!</Link> */}
-          <Image 
-            source={images.logo}
-            className="w-[130px] h-[84px]"
-            resizeMode="contain"
-          />
+          <Text className="text-3xl text-white font-pblack">SERT Alert</Text>
 
           <Image
            source={images.siren}
            className="max-w[390px] max-full h-[300px]"
           resizeMode="contain"
           /> 
-          <View className="relative mt-7">
+          {/* <View className="relative mt-7">
             <Text className="text-3xl text-white font-bold text-center">SERT Alert</Text>
-          </View>
+          </View> */}
 
           <CustomButton 
             title="Enter"
-            handlePress={() => {}}
+            handlePress={() => router.push('/emergency')}
             containerStyles="w-full mt-7"
           />
           
