3eda2b9dda068c6a386e34778e5d7e3e295919a9
Koby Macale <koby.macale24@gmail.com>
2024-06-17 17:56:13 +0800

    Layout design change for emergency, learning modules and menu

--- SERT-Alert/app/(tabs)/learningModules.jsx
+++ SERT-Alert/app/(tabs)/learningModules.jsx
@@ -1,11 +1,19 @@
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView, ScrollView, Image } from 'react-native'
 import React from 'react'
+
+import { images } from '../../constants';
 
 const LearningModules = () => {
   return (
-    <View>
-      <Text>Learning Modules</Text>
-    </View>
+    <SafeAreaView className="bg-white h-full">
+      <ScrollView>
+        <View className="w-full justify-center min-h-[85vh] px-4 my-6">
+          <Image source={images.logo}
+          resizeMode='contain' className="w-[115px] h-[35px]"/>
+          <Text className="text-2xl text-black text-semibold mt-10 font-psemibold">Learn through our Learning modules here!</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
--- SERT-Alert/app/(tabs)/menu.jsx
+++ SERT-Alert/app/(tabs)/menu.jsx
@@ -1,14 +1,20 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { View, Text, SafeAreaView, ScrollView, Image } from 'react-native'
 import React from 'react'
+
+import { images } from '../../constants';
 
 const menu = () => {
   return (
-    <View>
-      <Text>Menu</Text>
-    </View>
+    <SafeAreaView className="bg-white h-full">
+      <ScrollView>
+        <View className="w-full justify-center min-h-[85vh] px-4 my-6">
+          <Image source={images.logo}
+          resizeMode='contain' className="w-[115px] h-[35px]"/>
+          <Text className="text-2xl text-black text-semibold mt-10 font-psemibold">The Menu for your options!</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
 export default menu
-
-const styles = StyleSheet.create({})
