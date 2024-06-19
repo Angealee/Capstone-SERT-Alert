09faf73cdd86aaf2d47cef9871a4bc231b6b543b
Koby Macale <koby.macale24@gmail.com>
2024-06-19 15:52:38 +0800

    creation of the FormField component and initial modifications

--- SERT-Alert/app/(tabs)/emergency.jsx
+++ SERT-Alert/app/(tabs)/emergency.jsx
@@ -7,7 +7,7 @@ const Emergency = () => {
   return (
     <SafeAreaView className="bg-white h-full">
       <ScrollView>
-        <View className="w-full justify-center min-h-[85vh] px-4 my-6">
+        <View className="w-full justify-center h-full[85vh] px-4 my-6">
           <Image source={images.logo}
           resizeMode='contain' className="w-[115px] h-[35px]"/>
           <Text className="text-2xl text-black text-semibold mt-10 font-psemibold">Report an Emergency!</Text>
