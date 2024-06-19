d67e61fba2c04c2275b2ca03ea66f8018dbe7fb4
Koby Macale <koby.macale24@gmail.com>
2024-06-19 16:28:20 +0800

    FieldlForm design added

--- SERT-Alert/components/FormField.jsx
+++ SERT-Alert/components/FormField.jsx
@@ -1,10 +1,15 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-const FormField = () => {
+const FormField = ({ title, value, placeholder, handleChangeText, otherStyles, ...props}) => {
   return (
-    <View className="space-y-2">
-      <Text className="text-base text-black-100 font-pmedium">Form Field</Text>
+    <View className={`space-y-2 ${otherStyles}`}>
+      <Text className="text-base text-black-100 font-pmedium">{title}</Text>
+
+        <View className="border-2 border-red-500 w-full h-16 px-4 bg-white-100 rounded-2xl focus:border-secondary items-center">
+
+        </View>
+
     </View>
   )
 }
