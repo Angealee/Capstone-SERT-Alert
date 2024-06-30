6a0f5546a264461503d7dc5420b2b2ee26f37bb6
Koby Macale <koby.macale24@gmail.com>
2024-06-30 17:38:43 +0800

    minimal modifications and adjustments

--- SERT-Alert/app/(auth)/sign-in.jsx
+++ SERT-Alert/app/(auth)/sign-in.jsx
@@ -22,9 +22,9 @@ const SignIn = () => {
   return (
     <SafeAreaView className="bg-white h-full">
       <ScrollView>
-        <View className="w-full justify-center h-full[85vh] px-4 my-6">
-          <Image source={images.logo}
-          resizeMode='contain' className="w-[115px] h-[35px]"/>
+        <View className="w-full justify-center min-h-[85vh] px-4 my-6">
+          {/* <Image source={images.logo}
+          resizeMode='contain' className="w-[115px] h-[35px]"/> */}
           <Text className="text-2xl text-black text-semibold mt-10 font-psemibold">Sign-In!</Text>
 
         <FormField 
