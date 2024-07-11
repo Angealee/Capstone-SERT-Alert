6e5d059864439e02a8b1bafeedab9b6b322c58d4
Koby Macale <koby.macale24@gmail.com>
2024-07-11 22:01:52 +0800

    Initial commit. Added a SERT Logo for now. Flatlist testing outut

--- SERT-Alert/app/(tabs)/learningModules.jsx
+++ SERT-Alert/app/(tabs)/learningModules.jsx
@@ -1,15 +1,23 @@
-import { View, Text, SafeAreaView, ScrollView, Image } from 'react-native'
+import { View, Text,ScrollView, Image, FlatList } from 'react-native'
 import React from 'react'
-
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { images } from '../../constants';
 
 const LearningModules = () => {
   return (
-    <SafeAreaView className="bg-white h-full">
-      <ScrollView>
+    <SafeAreaView>
+        <FlatList 
+          data={[{ id: 1 }]}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <Text className="text-3xl">{item.id}</Text>
+          )}
+
+        />
+        <ScrollView>
         <View className="w-full justify-center h-full[85vh] px-4 my-6">
-          <Image source={images.logo}
-          resizeMode='contain' className="w-[115px] h-[35px]"/>
+          <Image source={images.SERTlogo}
+          resizeMode='contain' className="w-[35px] h-[35px]"/>
           <Text className="text-2xl text-black text-semibold mt-10 font-psemibold">Learn through our Learning modules here!</Text>
         </View>
       </ScrollView>
