c3f2466c924839d971fca1816908958303878edb
Koby Macale <koby.macale24@gmail.com>
2024-07-13 19:33:10 +0800

    readded endpoint, platform, and projectId in appwrite.js because of a dependency error which I git revert changes to previous commits. Dependency erroris now resolved

--- SERT-Alert/lib/appwrite.js
+++ SERT-Alert/lib/appwrite.js
@@ -1,5 +1,8 @@
 export const appwriteConfig = {
     endpoint: 'https://cloud.appwrite.io/v1',
     platform: 'com.angeale.capstoneSERTalert',
-    projectId: '66923beb0025ee8308d2'
+    projectId: '66923beb0025ee8308d2',
+    databaseId: '669248d1001d9d29f489',
+    SERTmembersCollectionId: '66924a2d00099aee7ee5',
+    userReportsId: '66924bdb002786b10f25'
 }
