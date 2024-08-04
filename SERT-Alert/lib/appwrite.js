61bb92cf6947a2235f9e791eb9e3373f7ad7d923
Angealee <koby.macale24@gmail.com>
2024-08-04 09:28:44 +0800

    added a storageId

--- SERT-Alert/lib/appwrite.js
+++ SERT-Alert/lib/appwrite.js
@@ -4,5 +4,6 @@ export const appwriteConfig = {
     projectId: '66923beb0025ee8308d2',
     databaseId: '669248d1001d9d29f489',
     SERTmembersCollectionId: '66924a2d00099aee7ee5',
-    userReportsId: '66924bdb002786b10f25'
+    userReportsId: '66924bdb002786b10f25',
+    storageId: '66aed87b0020ea8d8e8d'
 }
