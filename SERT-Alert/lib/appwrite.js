fe988d7905ee0e4067cfee0ca1b6be224f92f116
Angealee <koby.macale24@gmail.com>
2024-08-06 21:40:49 +0800

    function for new user and function for signing in is complete

--- SERT-Alert/lib/appwrite.js
+++ SERT-Alert/lib/appwrite.js
@@ -1,4 +1,4 @@
-import { ID, Account, Client } from 'react-native-appwrite';
+import { ID, Avatars, Account, Client, Databases } from 'react-native-appwrite';
 
 export const config = {
     endpoint: 'https://cloud.appwrite.io/v1',
@@ -20,15 +20,52 @@ client
     .setPlatform(config.platform) // Your application ID or bundle ID.
 
     const account = new Account(client);
+    const avatars = new Avatars(client);
+    const databases = new Databases(client);
 
 // Register User
-export const createUser = () => {
-account.create(ID.unique(), 'me@example.com', 'password', 'Jane Doe')
-    .then(function (response) {
-        console.log(response);
-    }, function (error) {
+export const createUser = async (email, password, username) => {
+    try {
+        const newAccount = await account.create(
+            ID.unique(),
+            email,
+            password,
+            username
+        )
+
+        if(!newAccount) throw Error;
+
+        const avatarUrl = avatars.getInitials(username)
+
+        await signIn(email, password)
+
+        const newUser = await databases.createDocument(
+            config.databaseId,
+            config.sertuserCollectionId,
+            ID.unique(),
+            {
+                accountId: newAccount.$id,
+                email,
+                username,
+                avatar: avatarUrl
+            }
+        )
+
+        return newUser;
+    }   catch (error) {
         console.log(error);
-    });
+        throw new Error(error);
+    }
+}
+
+export async function signIn(email, password, username) {
+    try {
+        const session = await account.createEmailSession(email, password)
+
+        return session;
+    }   catch (error) {
+        throw new Error(error);
+    }
 }
 
 
