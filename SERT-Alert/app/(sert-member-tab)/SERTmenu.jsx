08de739f98109d545b8bcd1956e203b7bf0b6eeb
Angealee <koby.macale24@gmail.com>
2024-08-07 23:46:17 +0800

    removed the signin button in SERT member UI

--- SERT-Alert/app/(sert-member-tab)/SERTmenu.jsx
+++ SERT-Alert/app/(sert-member-tab)/SERTmenu.jsx
@@ -16,18 +16,6 @@ const menu = () => {
           
           <Text className="text-2xl text-black text-semibold mt-10 font-psemibold">The Menu for your options!</Text>
           
-          <CustomButton 
-            title="Sign In"
-            handlePress={() => router.push('/sign-in')}
-            containerStyles="w-full mt-7"
-          />
-
-        </View>
-
-        <View className='justify-center flex-row'>
-          <Text className='text-lg text-gray-100 font-pregular'>
-            Are you a SERT Member?
-          </Text>
         </View>
 
         <View className='justify-center flex-row'>
