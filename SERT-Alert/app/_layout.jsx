5638aa421698bc30baaeb0fc657d4601d5bf73ed
Angealee <koby.macale24@gmail.com>
2024-10-23 18:37:29 +0800

    removed global provider codes in _layout

--- SERT-Alert/app/_layout.jsx
+++ SERT-Alert/app/_layout.jsx
@@ -2,7 +2,6 @@ import { StyleSheet, Text, View } from 'react-native'
 import { SplashScreen, Slot, Stack } from 'expo-router';
 import { useFonts } from 'expo-font'
 import { useEffect } from 'react';
-import GlobalProvider from '../context/GlobalProvider'; 
 SplashScreen.preventAutoHideAsync();
 
 const RootLayout = () => {
@@ -26,7 +25,6 @@ const RootLayout = () => {
   if(!fontsLoaded && !error) return null;
 
   return (
-  <GlobalProvider>
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" options={{headerShown: false}} />
       <Stack.Screen name="(auth)" options={{headerShown: false}} />
@@ -34,7 +32,6 @@ const RootLayout = () => {
       <Stack.Screen name="(sert-member-tab)" options={{headerShown: false}} />
       {/* <Stack.Screen name="/search/[query]" options={{headerShown: false}} /> */}
     </Stack>
-  </GlobalProvider>
   )
 }
 
